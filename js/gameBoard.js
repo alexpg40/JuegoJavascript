40ed11c8679d9d812c598ab0b23e56719f7499e3
alexpg40 <alexpg40@educantabria.es>
2022-01-18 17:11:39 +0100

    cambiado el tamaño otra vez

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -10,8 +10,8 @@ export default class gameBoard {
         let size = this.getSize;
         let board = document.createElement('div');
         board.style.display = 'grid';
-        board.style.gridTemplateColumns = `repeat(${size}, 75px)`;
-        board.style.gridTemplateRows = `repeat(${size}, 75px)`;
+        board.style.gridTemplateColumns = `repeat(${size}, 50px)`;
+        board.style.gridTemplateRows = `repeat(${size}, 50px)`;
         board.style.gridGap = "10px";
         for (let y = size -1;  y >= 0; y--) {
             for (let x = 0; x < size; x++) {
